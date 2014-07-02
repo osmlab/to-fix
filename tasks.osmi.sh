2d5689bef08b509b14213acb43fd6a83a60bfd77
Aaron Lidman <aaronlidman@gmail.com>
2014-07-01 19:55:44 -0700

    Put them in the right place

--- tasks.osmi.sh
+++ tasks.osmi.sh
@@ -1,37 +1,37 @@
-echo "
-    COPY (select wkb_geometry from intersection_lines) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > intersection_lines.csv
+mkdir osmi-tasks
 
 echo "
-    COPY (select wkb_geometry from intersections) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > intersections.csv
+    COPY (select ogc_fid, wkb_geometry from intersection_lines) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/intersection_lines.csv
 
 echo "
-    COPY (select wkb_geometry from role_mismatch) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > role_mismatch.csv
+    COPY (select ogc_fid, wkb_geometry from intersections) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/intersections.csv
 
 echo "
-    COPY (select wkb_geometry from role_mismatch_hull) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > role_mismatch_hull.csv
+    COPY (select ogc_fid, wkb_geometry from role_mismatch) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/role_mismatch.csv
 
 echo "
-    COPY (select wkb_geometry from unconnected_major1) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > unconnected_major1.csv
+    COPY (select ogc_fid, wkb_geometry from role_mismatch_hull) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/role_mismatch_hull.csv
 
 echo "
-    COPY (select wkb_geometry from unconnected_major2) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > unconnected_major2.csv
+    COPY (select ogc_fid, wkb_geometry from unconnected_major1) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/unconnected_major1.csv
 
 echo "
-    COPY (select wkb_geometry from unconnected_major5) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > unconnected_major5.csv
+    COPY (select ogc_fid, wkb_geometry from unconnected_major2) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/unconnected_major2.csv
 
 echo "
-    COPY (select wkb_geometry from unconnected_minor1) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > unconnected_minor1.csv
+    COPY (select ogc_fid, wkb_geometry from unconnected_major5) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/unconnected_major5.csv
 
 echo "
-    COPY (select wkb_geometry from unconnected_minor2) to stdout DELIMITER ',' CSV;
-" | psql -U postgres osmi > unconnected_minor2.csv
+    COPY (select ogc_fid, wkb_geometry from unconnected_minor1) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/unconnected_minor1.csv
 
-# chop them up and name then with unique reproduceable hashes
+echo "
+    COPY (select ogc_fid, wkb_geometry from unconnected_minor2) to stdout DELIMITER ',' CSV;
+" | psql -U postgres osmi > osmi-tasks/unconnected_minor2.csv
