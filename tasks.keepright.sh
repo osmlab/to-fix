40084046cd56968278ddc3ae45b2dfc973190bcc
Aaron Lidman <aaronlidman@gmail.com>
2014-07-10 19:47:06 -0400

    WKT

--- tasks.keepright.sh
+++ tasks.keepright.sh
@@ -1,19 +1,19 @@
 mkdir keepright-tasks
 
 echo "
-    COPY (select object_type, object_id, wkb_geometry from deadendoneway) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from deadendoneway) to stdout DELIMITER ',' CSV;
 " | psql -U postgres keepright > keepright-tasks/deadendoneway.csv
 
 echo "
-    COPY (select object_type, object_id, wkb_geometry from impossibleangle) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from impossibleangle) to stdout DELIMITER ',' CSV;
 " | psql -U postgres keepright > keepright-tasks/impossibleangle.csv
 
 echo "
-    COPY (select object_type, object_id, wkb_geometry from mixedlayer) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from mixedlayer) to stdout DELIMITER ',' CSV;
 " | psql -U postgres keepright > keepright-tasks/mixedlayer.csv
 
 echo "
-    COPY (select object_type, object_id, wkb_geometry from nonclosedways) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from nonclosedways) to stdout DELIMITER ',' CSV;
 " | psql -U postgres keepright > keepright-tasks/nonclosedways.csv
 
 for a in $(ls keepright-tasks/*.csv);
@@ -23,7 +23,7 @@ do
     count=0
     while read line
     do
-        echo $line > keepright-tasks/$dirname/$count.json
+        echo $line > keepright-tasks/$dirname/$count
         count=$(($count + 1))
     done < $a
 done
--- tasks.osmi.sh
+++ tasks.osmi.sh
@@ -1,39 +1,39 @@
 mkdir osmi-tasks
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from intersection_lines) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from intersection_lines) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/intersection_lines.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from intersections) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from intersections) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/intersections.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from role_mismatch) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from role_mismatch) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from role_mismatch_hull) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from role_mismatch_hull) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch_hull.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from unconnected_major1) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from unconnected_major1) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major1.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from unconnected_major2) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from unconnected_major2) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major2.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from unconnected_major5) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from unconnected_major5) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major5.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from unconnected_minor1) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from unconnected_minor1) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_minor1.csv
 
 echo "
-    COPY (select ogc_fid, wkb_geometry from unconnected_minor2) to stdout DELIMITER ',' CSV;
+    COPY (select ogc_fid, ST_AsText(wkb_geometry) from unconnected_minor2) to stdout DELIMITER ',' CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_minor2.csv
 
 for a in $(ls osmi-tasks/*.csv);
@@ -43,7 +43,7 @@ do
     count=0
     while read line
     do
-        echo $line > osmi-tasks/$dirname/$count.json
+        echo $line > osmi-tasks/$dirname/$count
         count=$(($count + 1))
     done < $a
 done
