e046cfdb732bb6676eae730812d3aadff086d1d4
Aaron Lidman <aaronlidman@gmail.com>
2014-07-13 22:19:53 -0400

    Add osmi endpoint

--- index.js
+++ index.js
@@ -10,11 +10,51 @@ http.createServer(router).listen(port, function() {
     console.log('running on port ' + port);
 });
 
-router.addRoute('/error/:db/:table', {
+router.addRoute('/error/keepright/:table', {
     GET: function(req, res, opts) {
-        // serves a random item
         var query = 'select object_type, object_id, ST_AsText(wkb_geometry) from ' + opts.table + ' order by random() limit 1;';
-        quick_query(opts.db, query, function(err, result) {
+        quick_query('keepright', query, function(err, result) {
+            if (err) {
+                console.log(err);
+                res.writeHead(404);
+                return res.end('ERROR');
+            }
+            res.end(JSON.stringify(result));
+        });
+    },
+    POST: function(req, res, opts) {
+        // lets not for now
+    }
+});
+
+router.addRoute('/error/osmi/:table', {
+    GET: function(req, res, opts) {
+        var query = '';
+        switch (opts.table) {
+            case 'intersections' || 'intersection_lines':
+                query = 'select rel_id ';
+                break;
+            // case role_mismatch:
+
+            //     break;
+            // case role_mismatch_hull:
+
+            //     break;
+            case 'unconnected_major1' ||
+                'unconnected_major2' ||
+                'unconnected_major5' ||
+                'unconnected_minor1' ||
+                'unconnected_minor2':
+                    query = 'select way_id, node_id, error_desc ';
+                    break;
+            default:
+                console.log('no hit');
+                res.writeHead(404);
+                return res.end('ERROR');
+        }
+
+        query += ' from ' + opts.table + ' order by random() limit 1;';
+        quick_query('osmi', query, function(err, result) {
             if (err) return console.log(err);
             res.end(JSON.stringify(result));
         });
