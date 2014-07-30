ad7b5b08eba60d6f184b6590b305db19473d1abb
Aaron Lidman <aaronlidman@gmail.com>
2014-07-30 15:28:42 -0400

    HEADERS

--- tasks.keepright.sh
+++ tasks.keepright.sh
@@ -1,17 +1,17 @@
 mkdir keepright-tasks
 
 echo "
-    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from deadendoneway) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from deadendoneway) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres keepright > keepright-tasks/deadendoneway.csv
 
 echo "
-    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from impossibleangle) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from impossibleangle) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres keepright > keepright-tasks/impossibleangle.csv
 
 echo "
-    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from mixedlayer) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from mixedlayer) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres keepright > keepright-tasks/mixedlayer.csv
 
 echo "
-    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from nonclosedways) to stdout DELIMITER ',' CSV;
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from nonclosedways) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres keepright > keepright-tasks/nonclosedways.csv
