643364bf6fdacfc5ea539170dba9568ea41521da
Aaron Lidman <aaronlidman@gmail.com>
2014-07-31 11:15:48 -0400

    Use redis correctly, somewhat

--- csv2redis.js
+++ csv2redis.js
@@ -1,22 +1,22 @@
 var fs = require('fs'),
     md5 = require('MD5'),
     redis = require('redis'),
-    csv = require('csv-parser'),
-    argv = require('minimist')(process.argv.slice(2));
+    csv = require('csv-parser');
 
 var client = redis.createClient();
+var task = process.argv[2].split('.')[0];
 
 client.on('error', function(err) {
     console.log('err', err);
 });
 
-var database = argv._[0].split('.')[0];
-
-fs.createReadStream(argv._[0])
+fs.createReadStream(process.argv[2])
     .pipe(csv())
     .on('data', function(data) {
         data = JSON.stringify(data);
-        client.set(database + '/' + md5(data), data);
+        var key = md5(data);
+        client.sadd(task, key);
+        client.hset(task + ':' + key, 'error', data);
     })
     .on('end', function() {
         client.quit();
