763205bd961feddd54842a7107257fb3cbf88c6d
Aaron Lidman <aaronlidman@gmail.com>
2014-07-31 20:48:33 -0400

    Add highwaywater task

--- tasks.keepright.sh
+++ tasks.keepright.sh
@@ -15,3 +15,7 @@ echo "
 echo "
     COPY (select object_type, object_id, ST_AsText(wkb_geometry) from nonclosedways) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres keepright > keepright-tasks/nonclosedways.csv
+
+echo "
+    COPY (select object_type, object_id, ST_AsText(wkb_geometry) from highwaywater) to stdout DELIMITER ',' HEADER CSV;
+" | psql -U postgres keepright > keepright-tasks/highwaywater.csv
