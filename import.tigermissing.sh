a0214ab8c83287a05086e9c921e970b458fa3d0d
Aaron Lidman <aaronlidman@gmail.com>
2014-08-06 09:55:42 -0400

    Add commas to each line

--- import.tigermissing.sh
+++ import.tigermissing.sh
@@ -1,7 +1,7 @@
 curl -f "http://trafficways.org/obsolete/missing.json.gz" -o missing.json.gz
 sudo ungzip missing.json.gz
 
-split -l 10000 missing.json chunks-
+split -l 100000 missing.json chunks-
 
 sudo -u postgres createdb -U postgres -T template_postgis -E UTF8 tigermissing
 
@@ -9,12 +9,21 @@ sudo -u postgres createdb -U postgres -T template_postgis -E UTF8 tigermissing
 echo '{"type":"FeatureCollection","features":[' > head
 echo ']}' > tail
 
+# add commas to each line
+for f in chunks-*;
+    do
+        awk '{print $0","}' $f > $f.commas
+        rm -rf $f
+    done
+
+# add featurecollection head and tail
 for f in chunks-*;
     do
         cat head $f tail > $f.json
         rm -f $f;
     done
 
+# insert each chunk into postgis
 for f in chunks-*.json;
     do
         sudo -u postgres ogr2ogr -update -append -f PostgreSQL PG:dbname=tigermissing $f;
