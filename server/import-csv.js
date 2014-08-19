3b61e6249ea45b61bf5bacf9a770aedfa920e6de
Aaron Lidman <aaronlidman@gmail.com>
2014-08-19 14:20:32 -0400

    Pipe in CSVs

--- server/import-csv.js
+++ server/import-csv.js
@@ -3,21 +3,41 @@ var fs = require('fs'),
     levelup = require('levelup'),
     path = require('path');
 
-var task = path.basename(process.argv[2]).split('.')[0],
+if (process.stdin.isTTY) {
+    if (process.argv[2] === undefined) {
+        return console.log('file argument required \n`node import-csv.js [file].csv`');
+    }
+    loadTask(process.argv[2]);
+} else {
+    process.stdin.on('readable', function() {
+        var buf = process.stdin.read();
+        if (buf === null) return;
+        buf.toString().split('\n').forEach(function(file) {
+            if (file.length) loadTask(file);
+        });
+    });
+}
+
+function loadTask(fileLoc) {
+    var task = path.basename(fileLoc).split('.')[0],
     db = levelup('./' + task + '.ldb'),
     count = 0;
 
-fs.createReadStream(process.argv[2])
-    .pipe(csv())
-    .on('data', function(data) {
-        // should really batch these
-        db.put(count, JSON.stringify(data), function (err) {
-            if (err) console.log('-- error --', err);
+    console.log('importing ' + task);
+
+    fs.createReadStream(fileLoc)
+        .pipe(csv())
+        .on('data', function(data) {
+            // should batch these
+            db.put(count, JSON.stringify(data), function (err) {
+                if (err) console.log('-- error --', err);
+            });
+            count++;
+        })
+        .on('end', function() {
+            setTimeout(function() {
+                db.close();
+                console.log('done with ' + task + '. ' + count + ' items imported');
+            }, 5000);
         });
-        count++;
-    })
-    .on('end', function() {
-        setTimeout(function() {
-            db.close();
-        }, 5000);
-    });
+}
