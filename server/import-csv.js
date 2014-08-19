f64d9dabc59b93bd565600e1691c8f40855101ad
Aaron Lidman <aaronlidman@gmail.com>
2014-08-19 08:21:21 -0400

    Cleanup

--- server/import-csv.js
+++ server/import-csv.js
@@ -7,21 +7,17 @@ var task = path.basename(process.argv[2]).split('.')[0],
     db = levelup('./' + task + '.ldb'),
     count = 0;
 
-var done = 0;
-
 fs.createReadStream(process.argv[2])
     .pipe(csv())
     .on('data', function(data) {
         // should really batch these
         db.put(count, JSON.stringify(data), function (err) {
             if (err) console.log('-- error --', err);
-            done++;
         });
         count++;
     })
     .on('end', function() {
         setTimeout(function() {
-            console.log(done);
-            console.log(count-done);
-        }, 2000);
+            db.close();
+        }, 5000);
     });
