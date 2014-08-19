5b6dbfb61d1fcfaa0ca8892393b3ecbcab43a6f3
Aaron Lidman <aaronlidman@gmail.com>
2014-08-18 21:42:36 -0400

    No redis

--- server/import-csv.js
+++ server/import-csv.js
@@ -1,31 +1,27 @@
 var fs = require('fs'),
-    md5 = require('MD5'),
-    redis = require('redis'),
     csv = require('csv-parser'),
     levelup = require('levelup'),
     path = require('path');
 
 var task = path.basename(process.argv[2]).split('.')[0],
-    client = redis.createClient(),
-    db = levelup('./' + task + '.ldb');
+    db = levelup('./' + task + '.ldb'),
+    count = 0;
 
-client.on('error', function(err) {
-    console.log('err', err);
-});
+var done = 0;
 
 fs.createReadStream(process.argv[2])
     .pipe(csv())
     .on('data', function(data) {
-        var key = md5(JSON.stringify(data));
-        try {
-            client.sadd(task, key);
-            db.put(key, data, function (err) {
-                console.log('put ' + key);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+        // should really batch these
+        db.put(count, JSON.stringify(data), function (err) {
+            if (err) console.log('-- error --', err);
+            done++;
+        });
+        count++;
     })
     .on('end', function() {
-        client.quit();
+        setTimeout(function() {
+            console.log(done);
+            console.log(count-done);
+        }, 2000);
     });
