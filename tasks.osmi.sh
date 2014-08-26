3b4567202b44f86bd4c5ce2e029cbcbe41705e18
Aaron Lidman <aaronlidman@gmail.com>
2014-08-26 16:17:18 -0400

    Select the most relevant columns

--- tasks.osmi.sh
+++ tasks.osmi.sh
@@ -1,19 +1,19 @@
 mkdir osmi-tasks
 
 echo "
-    COPY (select way_id, node_id from intersection_lines) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from intersection_lines) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/intersection_lines.csv
 
 echo "
-    COPY (select way_id, node_id from intersections) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from intersections) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/intersections.csv
 
 echo "
-    COPY (select way_id, node_id from role_mismatch) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from role_mismatch) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch.csv
 
 echo "
-    COPY (select way_id, node_id from role_mismatch_hull) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from role_mismatch_hull) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch_hull.csv
 
 echo "
