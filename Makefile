5d4961017beb3ecf5716e237a0905514f274d970
Aaron Lidman <aaronlidman@gmail.com>
2014-09-30 16:46:28 -0400

    Stats escape

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ install:
 	sh install.sh
 load:
 	sh load.sh
+stats:
+	sh getStats.sh
--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ apt-get -y update
 apt-get install -y zip git vim htop bzip2 curl libleveldb-dev nodejs npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 npm install
+apt-get install -y s3cmd
+s3cmd --configure
 
 ulimit -n 1000000
