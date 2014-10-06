d87badcb1e2a8d7afcf313b2c4b4e9eef1470b8f
Aaron Lidman <aaronlidman@gmail.com>
2014-10-06 19:25:58 -0400

    Persistent db connection, let's see what happens

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var http = require('http'),
     Route = require('routes-router'),
     levelup = require('levelup'),
-    leveldown = require('leveldown');
+    leveldown = require('leveldown'),
+    fs = require('fs');
+
+var level = {};
+
+fs.readdirSync('./').filter(function(item) {
+   return item.indexOf('.ldb') > -1;
+}).forEach(function(ldb) {
+    levelup(ldb, {
+        createIfMissing: false,
+        max_open_files: 500
+    }, function(err, db) {
+        level[ldb] = db;
+    });
+});
 
 var router = Route();
 var headers = {
@@ -49,18 +63,8 @@ router.addRoute('/fixed/:error', {
             if (body.user && body.state._id) {
                 track(opts.params.error, body.user, 'fixed', body.state);
 
-                var location = './' + opts.params.error + '.ldb';
-                levelup(location, {
-                    createIfMissing: false,
-                    max_open_files: 500
-                }, function(err, db) {
-                    if (err) {
-                        if (db && !db.isClosed()) db.close();
-                        return console.log('/fixed error', err);
-                    }
-                    db.del(body.state._id, function() {
-                        db.close();
-                    });
+                level[opts.params.error + '.ldb'].del(body.state._id, function(err) {
+                    if (err) console.log('delete', err);
                 });
 
                 res.writeHead(200, headers);
@@ -71,56 +75,35 @@ router.addRoute('/fixed/:error', {
 });
 
 function getNextItem(error, res, callback) {
-    var location = './' + error + '.ldb';
-    levelup(location, {
-        createIfMissing: false,
-        max_open_files: 500
-    }, function(err, db) {
-        if (err !== null) {
-            // we still get empty errors, {}
-            if (db && !db.isClosed()) db.close();
-            console.log('Database error', err);
-            return callback('Database error');
-        }
+    var newKey = (+new Date() + lockperiod).toString() + Math.random().toString().slice(1, 4);
 
-        var newKey = (+new Date() + lockperiod).toString() + Math.random().toString().slice(1, 4);
-
-        db.createReadStream({limit: 1, lt: (+new Date())})
-            .on('data', function(data) {
-                db.del(data.key, function() {
-                    db.put(newKey, data.value, function(err) {
-                        db.close();
-                        data.key = newKey;
-                        data.value = JSON.parse(data.value);
-                        return callback(null, data);
-                    });
+    var db = level[error + '.ldb'];
+    db.createReadStream({limit: 1, lt: (+new Date())})
+        .on('data', function(data) {
+            db.del(data.key, function() {
+                db.put(newKey, data.value, function(err) {
+                    if (err) console.log('put', err);
+                    data.key = newKey;
+                    data.value = JSON.parse(data.value);
+                    return callback(null, data);
                 });
-            })
-            .on('error', function(err) {
-                db.close();
-                console.log('CreateReadStream error', err);
-                return callback('Something wrong with the database');
             });
-    });
+        })
+        .on('error', function(err) {
+            console.log('CreateReadStream error', err);
+            return callback('Something wrong with the database');
+        });
 }
 
 function track(error, user, action, value) {
     // value must be an object
-    var trackingDb = './' + error + '-tracking.ldb';
     var key = +new Date() + ':' + user;
     value._action = action;
     value = JSON.stringify(value);
-    levelup(trackingDb, {
-        max_open_files: 500
-    }, function(err, db) {
-        if (err) {
-            if (db && !db.isClosed()) db.close();
-            console.log('tracking error', err);
-            return;
-        }
-        db.put(key, value, function(err) {
-            db.close();
-        });
+
+    var db = level[error + '-tracking.ldb'];
+    db.put(key, value, function(err) {
+        if (err) console.log('put', err);
     });
 }
 
