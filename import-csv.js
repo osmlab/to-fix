f40d2536a2e306f1d77ab2aab3828091553a204c
Aaron Lidman <aaronlidman@gmail.com>
2014-11-10 20:31:26 -0500

    Check for fixed items from a file instead

--- import-csv.js
+++ import-csv.js
@@ -1,35 +1,45 @@
 var fs = require('fs'),
+    path = require('path'),
+    readline = require('readline'),
     csv = require('csv-parser'),
     levelup = require('levelup'),
-    path = require('path'),
     key = require('./lib/key.js');
 
 if (process.stdin.isTTY) {
     if (process.argv[2] === undefined) {
-        return console.log('file argument required \n`node import-csv.js [source csv] [fixed json]`');
+        return console.log('file argument required \n`node import-csv.js [source csv]`');
     }
-    loadTask(process.argv[2], process.argv[3]);
+    loadTask(process.argv[2]);
 } else {
     process.stdin.on('readable', function() {
         var buf = process.stdin.read();
         if (buf === null) return;
         buf.toString().split('\n').forEach(function(file) {
-            if (file.length) loadTask(file, process.argv[2]);
+            if (file.length) loadTask(fileLoc, process.argv[2]);
         });
     });
 }
 
-function loadTask(fileLoc, fixedLoc) {
+function loadTask(fileLoc) {
     var task = path.basename(fileLoc).split('.')[0],
         db = levelup('./ldb/' + task + '.ldb'),
         count = 0;
 
-    // load list of IDs that are already fixed
-    var fixed_list = [];
-    if (fixedLoc !== undefined) {
-        if (fixedLoc[0] !== '/') fixedLoc = './' + fixedLoc;
-        fixed_list = require(fixedLoc);
-        fixed_list = fixed_list[task + '.ldb'] ? fixed_list[task + '.ldb'] : [];
+    if (fs.readdirSync('./fixed').indexOf(task) > -1) {
+        var fixed_list = [];
+
+        var rl = readline.createInterface({
+            input: fs.createReadStream('./fixed/' + task),
+            output: new require('stream')
+        });
+
+        rl.on('line', function(line) {
+            fixed_list.push(line);
+        });
+
+        rl.on('end', function() {
+            console.log(fixed_list.length);
+        });
     }
 
     var tracking = levelup('./ldb/' + task + '-tracking.ldb');
@@ -43,7 +53,7 @@ function loadTask(fileLoc, fixedLoc) {
             var object_hash = key.hashObject(data);
             if (fixed_list.indexOf(object_hash) === -1) {
                 // item is not fixed
-                var object_id = key.compose(1, object_hash);
+                var object_id = key.compose(0, object_hash);
                 db.put(object_id, JSON.stringify(data), function (err) {
                     if (err) console.log('-- error --', err);
                 });
