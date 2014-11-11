f94e96267ef4a6d06cfb0d902da76e35e2b9f812
Tom Lee <thomas.j.lee@gmail.com>
2014-11-11 17:01:51 -0500

    better logging & comments

--- prioritize.js
+++ prioritize.js
@@ -13,24 +13,32 @@ if (process.stdin.isTTY) {
         return console.log('file arguments are required\n`node prioritize.js [leveldb] [geojson1 geojson2 .. ]`');
     }
 
-    // load geojson files
+    // process command line params, load geojson files
     var geojson = [];
     process.argv        
         .forEach(function(elem, i) {
             if (i <= 2) {
-                // skip these -- not using .filter() to make async logic simpler
+                // skip require params -- choosing not to use .filter() to make async logic simpler below
                 return true;
             }
+
+            // if using overlapping geojson files, this flag might be useful. otherwise, no.
             if (elem === '--slow') {
                 quickMode = false;
             }
             else {
-                console.log('+ loading ' + elem);
                 fs.readFile(elem, function(err, data) {
-                    if (err) console.log('# Error loading GeoJSON file ' + elem);
-                    console.log('- Adding GeoJSON file ' + elem);
-                    geojson.push(JSON.parse(data));
-                
+                    if (err) console.log('# Error loading GeoJSON file ' + elem);                
+                    var boundary = JSON.parse(data);
+                    if((boundary===null) || (!boundary.features)) {
+                        console.log('# failed to load valid GeoJSON from ' + elem);
+                    }
+                    else {
+                        geojson.push(boundary);  
+                        console.log('- loaded GeoJSON file ' + elem);  
+                    }
+                    
+                    // if all params have been loaded, begin main processing
                     if(i === process.argv.length-1) ProcessLevelDB();
                 });                
             }
@@ -38,12 +46,12 @@ if (process.stdin.isTTY) {
 }
 
 function ProcessLevelDB(){
-    console.log('- Opening LevelDB database ' + process.argv[2]);
+    console.log('- opening leveldb database ' + process.argv[2]);
     levelup(process.argv[2], function(err, db) {
         if (err) throw(err);
 
         var maxOverlaps = 0;
-        console.log('- Checking for geometry overlap')
+        console.log('- checking for geometry overlap')
         db.createReadStream()
             .on('data', function(data) {
                 data.value = JSON.parse(data.value);
@@ -55,7 +63,7 @@ function ProcessLevelDB(){
                 var overlaps = 0;
                 geojson.forEach(function(poly) {                    
                     if (!data.value.st_astext) {              
-                        console.log('# Missing geometry (st_astext) for key ' + data.key);
+                        console.log('# missing geometry (st_astext) for key ' + data.key);
                         return false;
                     } 
 
@@ -66,29 +74,39 @@ function ProcessLevelDB(){
                 });
 
                 // track maximum number of overlaps
-                maxOverlaps = Math.max(maxOverlaps, overlaps);
+                maxOverlaps = Math.max(maxOverlaps, overlaps);                
 
-                data.value.overlaps = overlaps;
+                data.value.overlaps = overlaps;                
                 db.put(data.key, JSON.stringify(data.value), function(err) {
-                    if (err) console.log('# Error saving key ' + data.key + '#' + overlaps);
+                    if (err) console.log('# error saving key ' + data.key + '#' + overlaps);
                 });                
             })
             .on('end', function(){
 
                 // iterate through all keys, adding (maxOverlaps - overlap) to each skipval
-                console.log('- Reordering tasks')
+                console.log('- reordering tasks')
+                var adjustmentsMade = 0;
+                
                 db.createReadStream()
                     .on('data', function(data) { 
-                        data.value = JSON.parse(data.value);                               
+                        data.value = JSON.parse(data.value);  
+                       
                         var keyComponents = key.decompose(data.key);
-                        var newKey = key.compose((keyComponents.skipval + (maxOverlaps - parseInt(data.overlaps))), keyComponents.hash);                    
+                        var newKey = key.compose((keyComponents.skipval + (maxOverlaps - parseInt(data.value.overlaps))), keyComponents.hash);                    
 
-                        // delete data.value.overlaps;
+                        if (parseInt(data.value.overlaps) > 0) {
+                            adjustmentsMade++;
+                        }
+
+                        delete data.value.overlaps;
                         db.put(newKey, JSON.stringify(data.value), function(err) {
-                            if (err) console.log('# Error saving key ' + newKey);                            
-                        })
-                    });
+                            if (err) console.log('# error saving key ' + newKey);                            
+                        });
 
+                    })
+                    .on('end', function() {
+                        console.log('- finished, reprioritized ' + adjustmentsMade + ' tasks')
+                    });
             });
 
     });
