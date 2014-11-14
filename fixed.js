0431cf3798b5934ad26caa30deea82bf2db6fea7
Tom Lee <thomas.j.lee@gmail.com>
2014-11-14 00:15:06 -0500

    nonworking attempt to avoid illegal filesystem operation -- need to determine what's causing illegal paths to be passed at all, though

--- fixed.js
+++ fixed.js
@@ -24,19 +24,32 @@ function fixed(callback) {
                 max_open_files: 500
             }, function(err, db) {
                 if (err) return console.log(err);
-                var file = fs.createWriteStream('./fixed/' + ldb.split('.ldb')[0]);
-                file.once('open', function() {
-                    db.createReadStream({lt: '0001'})
-                        .on('data', function(data) {
-                            file.write(key.decompose(data.key).hash + '\n');
-                        })
-                        .on('end', function(data) {
-                            file.end();
-                            db.close(function(err){
-                                qcallback(err);   
-                            });                        
-                        });
+                // test to ensure this is a file, not a directory
+                // unclear why this is newly necessary, but it is
+                var filePath = './fixed/' + ldb.split('.ldb')[0];
+                fs.stat(filePath, function(err, stats) {
+                    if (err) return qcallback(err);
+                    if (stats.isFile()) {
+                        var file = fs.createWriteStream(filePath);
+                        file.once('open', function() {
+                            db.createReadStream({lt: '0001'})
+                                .on('data', function(data) {
+                                    file.write(key.decompose(data.key).hash + '\n');
+                                })
+                                .on('end', function(data) {
+                                    file.end();
+                                    db.close(function(err){
+                                        qcallback(err);   
+                                    });                        
+                                });
+                        });                        
+                    }
+                    else {
+                        qcallback(null);
+                    }
+
                 });
+                
             });
         });
     });
