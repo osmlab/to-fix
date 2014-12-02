d25492e3d88709d732bccafafb143148922b8167
Aaron Lidman <aaronlidman@gmail.com>
2014-12-02 11:01:36 -0500

    Use express instead

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var http = require('http'),
-    Route = require('routes-router'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
     fs = require('fs'),
     key = require('./lib/key.js');
+
+app.use(bodyParser.json());
 
 function debug(x){
     process.stderr.write(x + '\n');
 }
 
-var router = Route();
 var headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, GET',
@@ -15,54 +18,36 @@ var headers = {
     'Access-Control-Max-Age': '86400'
 };
 var port = 3001;
-var lockperiod = 10*60;
 
-function runPostInit(){
-    http.createServer(router).listen(port, function() {
-        debug('running on port ' + port);
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.post('/error/:error', function(req, res) {
+    getNextItem(opts.params.error, res, function(err, kv) {
+        if (err) {
+            debug('/error route', err);
+            return error(res, 500, err);
+        }
+        track(opts.params.error, req.body.user, 'got', {_id: kv.key});
+        res.writeHead(200, headers);
+        return res.end(JSON.stringify(kv));
     });
-}
+});
 
-router.addRoute('/error/:error', {
-    POST: function(req, res, opts) {
-        var body = '';
-        req.on('data', function(data) {
-            body += data;
-        });
-        req.on('end', function() {
-            body = JSON.parse(body);
-            getNextItem(opts.params.error, res, function(err, kv) {
-                if (err) {
-                    debug('/error route', err);
-                    return error(res, 500, err);
-                }
-                track(opts.params.error, body.user, 'got', {_id: kv.key});
-                res.writeHead(200, headers);
-                return res.end(JSON.stringify(kv));
-            });
-        });
+app.post('/fixed/:error', function(req, res) {
+    var body = req.body;
+    if (body.user && body.state._id) {
+        track(opts.params.error, body.user, 'fixed', body.state);
+
+        // mark the thing as done
+
+        res.writeHead(200, headers);
+        return res.end('');
     }
 });
 
-router.addRoute('/fixed/:error', {
-    POST: function(req, res, opts) {
-        var body = '';
-        req.on('data', function(data) {
-            body += data;
-        });
-        req.on('end', function() {
-            body = JSON.parse(body);
-            if (body.user && body.state._id) {
-                track(opts.params.error, body.user, 'fixed', body.state);
-
-                // mark the thing as done
-
-                res.writeHead(200, headers);
-                return res.end('');
-            }
-        });
-    }
-});
+app.listen(port);
 
 function getNextItem(error, res, callback) {
     if (error === (undefined || 'undefined')) {
