5384c9b5d450b3e64deff6116d1b4bf40438858a
Aaron Lidman <aaronlidman@gmail.com>
2015-01-07 10:52:23 -0500

    Move more to keepright loader

--- loaders/keepright.js
+++ loaders/keepright.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 
-var _ = require('underscore'),
-    map = require('../lib/map.js'),
+var core = require('../lib/core'),
+    qs = require('querystring').parse(window.location.search.slice(1)),
+    _ = require('underscore'),
+    map = require('../lib/map'),
     omnivore = require('leaflet-omnivore'),
     querystring = require('querystring');
 
@@ -11,32 +13,41 @@ var templates = {
 
 var keepright = {};
 
+keepright.auth = ['osm'];
+
 keepright.next = function(callback) {
-    map();
+    map.init();
 
-    current._osm_object_type = current.object_type;
-    current._osm_object_id = current.object_id;
-    var full = current._osm_object_type == 'way' ? '/full' : '';
-
-    $.ajax({
-        url: 'https://www.openstreetmap.org/api/0.6/' + current._osm_object_type + '/' + current._osm_object_id + full,
-        dataType: 'xml',
-        success: function (xml) {
-            var layer = new L.OSM.DataLayer(xml).setStyle(featureStyle).addTo(featureGroup);
-            current._bounds = layer.getBounds();
-            window.map.fitBounds(current._bounds);
-            omnivore.wkt.parse(current.st_astext).addTo(featureGroup);
-
-            if (!$('#editbar').length) {
-                $('#main').append(templates.editbar());
-                keepright.bind(callback);
-            }
-        },
-        error: function(err) {
-            console.log(err);
-            if (callback) callback(err, current);
-        }
+    core.item(qs.error, function(data) {
+        console.log(qs);
+        console.log(window.current);
     });
+
+    // console.log(window.current);
+
+    // current._osm_object_type = current.object_type;
+    // current._osm_object_id = current.object_id;
+    // var full = current._osm_object_type == 'way' ? '/full' : '';
+
+    // $.ajax({
+    //     url: 'https://www.openstreetmap.org/api/0.6/' + current._osm_object_type + '/' + current._osm_object_id + full,
+    //     dataType: 'xml',
+    //     success: function (xml) {
+    //         var layer = new L.OSM.DataLayer(xml).setStyle(featureStyle).addTo(featureGroup);
+    //         current._bounds = layer.getBounds();
+    //         window.map.fitBounds(current._bounds);
+    //         omnivore.wkt.parse(current.st_astext).addTo(featureGroup);
+
+    //         if (!$('#editbar').length) {
+    //             $('#main').append(templates.editbar());
+    //             keepright.bind(callback);
+    //         }
+    //     },
+    //     error: function(err) {
+    //         console.log(err);
+    //         if (callback) callback(err, current);
+    //     }
+    // });
 };
 
 keepright.bind = function(callback) {
