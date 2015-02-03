a8c135fa74d1bf1c17881a173dbe3619dff38654
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-02-03 16:09:20 -0500

    switching indentation

--- loaders/bridge_height.js
+++ loaders/bridge_height.js
@@ -1,42 +1,42 @@
 var fs = require('fs');
 
 var querystring = require('querystring'),
-	qs = querystring.parse(window.location.search.slice(1));
+    qs = querystring.parse(window.location.search.slice(1));
 
 var core = require('../lib/core'),
-	map = require('../lib/map'),
-	editbar = require('../lib/editbar');
+    map = require('../lib/map'),
+    editbar = require('../lib/editbar');
  
  var bridge_height = {
- 	auth: ['osm']
+    auth: ['osm']
  }
 
  bridge_height.next = function() {
- 	map.init();
- 	editbar.init();
+    map.init();
+    editbar.init();
 
- 	core.item(qs.error, function() {
- 		console.log(current.item);
- 		L.marker([current.item.lat, current.item.lon])
- 			.addTo(window.map)
- 			.bindPopup('<table style="width:450px"> \
- 							<tr> \
- 								<td> Clearance Height </td> \
- 								<td>'+ current.item.height +'</td> \
- 							</tr> \
- 							<tr> \
- 								<td> Feature Carried </td> \
- 								<td>'+ current.item['Feature.Carried'] +'</td> \
- 							</tr> \
- 							<tr> \
- 								<td> Feature intersected </td> \
- 								<td>'+ current.item['Features.Intersected'] +'</dt> \
- 							</tr> \
- 						</table>', {maxWidth:500})
- 			.openPopup();
- 		window.map.setView([current.item.lat, current.item.lon], 16)
- 		current.item._bounds = window.map.getBounds();
- 	});
+    core.item(qs.error, function() {
+        console.log(current.item);
+        L.marker([current.item.lat, current.item.lon])
+            .addTo(window.map)
+            .bindPopup('<table style="width:450px"> \
+                            <tr> \
+                                <td> Clearance Height </td> \
+                                <td>'+ current.item.height +'</td> \
+                            </tr> \
+                            <tr> \
+                                <td> Feature Carried </td> \
+                                <td>'+ current.item['Feature.Carried'] +'</td> \
+                            </tr> \
+                            <tr> \
+                                <td> Feature intersected </td> \
+                                <td>'+ current.item['Features.Intersected'] +'</dt> \
+                            </tr> \
+                        </table>', {maxWidth:500})
+            .openPopup();
+        window.map.setView([current.item.lat, current.item.lon], 16)
+        current.item._bounds = window.map.getBounds();
+    });
  };
 
  module.exports = bridge_height;
