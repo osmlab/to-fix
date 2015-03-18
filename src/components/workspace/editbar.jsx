0ee6d8f63ac9afa1d8b7db2bc1d5058239e0e319
tristen <tristen.brown@gmail.com>
2015-03-18 11:07:09 -0400

    auth/non-auth state for edit task bar

--- src/components/workspace/editbar.jsx
+++ src/components/workspace/editbar.jsx
@@ -1,14 +1,17 @@
 'use strict';
 
 var React = require('react');
+var Reflux = require('reflux');
 var Router = require('react-router');
 var taskObj = require('../../mixins/task-item');
 var actions = require('../../actions/actions');
+var UserStore = require('../../stores/user');
 var Keys = require('react-keybinding');
 
 module.exports = React.createClass({
   mixins: [
     Router.State,
+    Reflux.connect(UserStore, 'user'),
     taskObj,
     Keys
   ],
@@ -39,18 +42,32 @@ module.exports = React.createClass({
 
   render: function() {
     var taskTitle = taskObj(this.getParams().task).title;
+    var taskActions = (
+      /* jshint ignore:start */
+      <nav className='tabs col12 clearfix'>
+        <a onClick={this.skip} className='col12 animate icon refresh'>Another task</a>
+      </nav>
+      /* jshint ignore:end */
+    );
+
+    if (this.state.user && this.state.user.auth) {
+      taskActions = (
+        /* jshint ignore:start */
+        <nav className='tabs col12 clearfix'>
+          <button onClick={this.edit} className='col4 animate unround'>Edit</button>
+          <button onClick={this.skip} className='col4 animate'>Skip</button>
+          <button onClick={this.fixed} className='col4 animate'>Fixed</button>
+        </nav>
+        /* jshint ignore:end */
+      );
+    }
+
     return (
       /* jshint ignore:start */
       <div className='editbar pin-bottomleft col12 pad4 z1'>
         <div className='dark round col6 margin3'>
-          <nav id='actions' className='tabs col12 clearfix'>
-            <button onClick={this.edit} className='fill-darken3 col4 animate unround'>Edit</button>
-            <button onClick={this.skip} className='fill-darken3 col4 animate'>Skip</button>
-            <button onClick={this.fixed} className='fill-darken3 col4 animate'>Fixed</button>
-          </nav>
-          <div className='fill-darken1 round-bottom col12 pad2x pad1y center strong inline'>
-            {taskTitle}
-          </div>
+          {taskActions}
+          <div className='fill-darken1 round-bottom col12 pad2x pad1y center strong inline'>{taskTitle}</div>
         </div>
       </div>
       /* jshint ignore:end */
