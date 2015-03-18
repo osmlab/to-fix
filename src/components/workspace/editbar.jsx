0aacaaea63ccc82592d9f0cc3faf0bf2d75d3ddc
tristen <tristen.brown@gmail.com>
2015-03-17 22:52:43 -0400

    task bar as buttons and add keybindings to controls

--- src/components/workspace/editbar.jsx
+++ src/components/workspace/editbar.jsx
@@ -3,25 +3,53 @@
 var React = require('react');
 var Router = require('react-router');
 var taskObj = require('../../mixins/task-item');
+var Keys = require('react-keybinding');
 
 module.exports = React.createClass({
   mixins: [
     Router.State,
-    taskObj
+    taskObj,
+    Keys
   ],
+
+  keybindings: {
+    'e': function() {
+      this.edit();
+    },
+    's': function() {
+      this.skip();
+    },
+    'f': function() {
+      this.fixed();
+    }
+  },
+
+  edit: function() {
+    console.log('edit');
+  },
+
+  skip: function() {
+    console.log('skip');
+  },
+
+  fixed: function() {
+    console.log('fixed');
+  },
 
   render: function() {
     var taskTitle = taskObj(this.getParams().task).title;
     return (
       /* jshint ignore:start */
-      <div id='editbar' className='pin-topleft pad1 z1'>
-        <div className='fill-darken3 dark round'>
-          <span className='pad2x strong quiet inline'>{taskTitle}</span>
-          <nav id='actions' className='fill-darken0 round-right tabs short'>
-            <a href='#' id='edit' className='animate unround'>Edit</a>
-            <a href='#' id='skip' className='keyline-left animate'>Skip</a>
-            <a href='#' id='fixed' className='keyline-left animate'>Fixed</a>
+      <div className='editbar pin-bottomleft col12 pad4 z1'>
+        <div className='dark round col6 margin3'>
+          <nav id='actions' className='fill-darken3 round-top tabs col12 clearfix'>
+            <button onClick={this.edit} className='col4 animate unround'>Edit</button>
+            <button onClick={this.skip} className='col4 animate'>Skip</button>
+            <button onClick={this.fixed} className='col4 animate'>Fixed</button>
           </nav>
+          <div className='fill-darken1 round-bottom col12 pad2x pad1y center strong inline'>
+            {taskTitle}
+          </div>
         </div>
       </div>
       /* jshint ignore:end */
