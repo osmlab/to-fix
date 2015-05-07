a325e799896502d3d7d0b07d82d8db3d5ee4922c
Aaron Lidman <aaronlidman@gmail.com>
2015-05-07 18:07:47 -0400

    Update krak

--- src/components/task.js
+++ src/components/task.js
@@ -4,6 +4,7 @@ require('mapbox.js');
 require('leaflet-osm');
 
 var omnivore = require('leaflet-omnivore');
+var wellknown = require('wellknown');
 
 var React = require('react');
 var Reflux = require('reflux');
@@ -63,7 +64,31 @@ module.exports = React.createClass({
       map.fitBounds(layer.getBounds(), { reset: true });
       this.geolocate(map.getCenter());
     } else if (task == 'rk') {
-      var layer = omnivore.wkt.parse(this.state.map.value.geom).addTo(taskLayer);
+      var geom = wellknown.parse(this.state.map.value.geom);
+
+      var circleOptions = {
+        stroke: false,
+        color: '#fff',
+        opacity: 0.1,
+        fillColor: '#03f',
+        fillOpacity: 0.5,
+        fill: true,
+        weight: 0,
+        radius: 5
+      };
+
+      if (geom.type == 'MultiPoint') {
+        geom.coordinates.forEach(function(coords) {
+          L.circleMarker([coords[1], coords[0]], circleOptions).addTo(taskLayer);
+        });
+      }
+
+      if (geom.type == 'Point') {
+        L.circleMarker([geom.coordinates[1], geom.coordinates[0]], circleOptions).addTo(taskLayer);
+      }
+
+      var layer = omnivore.wkt.parse(this.state.map.value.geom);
+        // create the layer but don't actually use it, only for getBounds
       map.fitBounds(layer.getBounds(), { reset: true });
       this.geolocate(map.getCenter());
     }
