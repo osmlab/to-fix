797df1e906ab6fcd13ecc6de23168be6824c2a68
Ajith Ranka <ajith@mapbox.com>
2016-10-19 17:51:51 +0530

    admin component

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,11 +1,9 @@
-'use strict';
+import React from 'react';
 
-var React = require('react');
-
-var actions = require('../actions/actions');
-var Addtask = require('./admin/add_task');
-var Showtask = require('./admin/show_task');
-var Edittask = require('./admin/edit_task');
+import actions from '../actions/actions';
+import Addtask from './admin/add_task';
+import Showtask from './admin/show_task';
+import Edittask from './admin/edit_task';
 
 const Admin = React.createClass({
   getInitialState: function() {
@@ -43,20 +41,20 @@ const Admin = React.createClass({
   },
   render: function() {
     return (
-        <div className='col12 clearfix scroll-styled'>
+      <div className='col12 clearfix scroll-styled'>
+        <div className='col6 pad2 dark'>
+         {(this.state.show_task_window) ? (<Showtask />) : ''}
+         {(this.state.add_task_window) ? (<Addtask />) : ''}
+         {(this.state.edit_task_window) ? (<Edittask />) : ''}
+        </div>
           <div className='col6 pad2 dark'>
-           {(this.state.show_task_window) ? (<Showtask />) : ''}
-           {(this.state.add_task_window) ? (<Addtask />) : ''}
-           {(this.state.edit_task_window) ? (<Edittask />) : ''}
-          </div>
-            <div className='col6 pad2 dark'>
-            <div className='pill'>
-              <a onClick={this.openShow_task} className='button pad2x quiet'>Show detail</a>
-              <a onClick={this.openEdit_task} className='button pad2x quiet'>Edit this task</a>
-              <a onClick={this.openAdd_task} className='button pad2x quiet'>Add a task</a>
-            </div>
+          <div className='pill'>
+            <a onClick={this.openShow_task} className='button pad2x quiet'>Show detail</a>
+            <a onClick={this.openEdit_task} className='button pad2x quiet'>Edit this task</a>
+            <a onClick={this.openAdd_task} className='button pad2x quiet'>Add a task</a>
           </div>
         </div>
+      </div>
     );
   }
 });
