9b55f2501e10abd72e0aaf793226180546123dad
Ajith Ranka <ajith@mapbox.com>
2016-10-19 17:03:56 +0530

    refactor the entry point

--- src/index.js
+++ src/index.js
@@ -1,67 +1,47 @@
-'use strict';
+import React from 'react';
+import { render } from 'react-dom';
+import qs from 'querystring';
+import { Router, hashHistory, RouterContext } from 'react-router';
+import Raven from 'raven-js';
 
-var React = require('react');
-
-//var Reflux = require('reflux');
-var qs = require('querystring');
-
-var Router = require('react-router');
-var Routes = require('./routes');
-var config = require('./config');
-var auth = require('./mixins/auth');
-var Raven = require('raven-js');
-
-var userStore = require('./stores/user_store.js');
+import routes from './routes';
+import * as config from './config';
+import auth from './mixins/auth';
+import userStore from './stores/user_store.js';
 
 Raven.config(config.raven, {
   whitelistUrls: ['osmlab.github.io/to-fix/']
 }).install();
 
-function doAsync(routes, params) {
-  var _this = this;
-
-  // Find all routes that require some async 'fetchData' work to be done before
-  // display, and turn their promises into one promise that works serially.
-  return routes.filter(function(route) {
-    return route.handler.fetchData;
-  }).reduce(function(memo, route) {
-    return memo.then(route.handler.fetchData.bind(_this, params));
-  }, Promise.resolve(true));
+// Find all routes that require some async 'fetchData' work to be done before
+// display, and turn their promises into one promise that works serially.
+const doAsync = (routes, params) => {
+  return routes
+    .filter((route) => route.component.fetchData)
+    .reduce(
+      (memo, route) => memo.then(route.component.fetchData.bind(this, params)),
+      Promise.resolve(true)
+    );
 }
-
-var router = Router.create({
-  scrollBehavior: false,
-  routes: Routes
-});
-
-// Main initial elements
-var el = document.getElementById('app');
-var loader = document.getElementById('loader');
 
 // Router will rewrite paths it doesn't know including
-// the path for OAuth. So check for oauth_token first.
+// the path for OAuth. So check for oauth_token first,
 // before initializing the router.
 if (window.location.search && !auth.authenticated()) {
-  var oauth_token = qs.parse(window.location.search.replace('?', '')).oauth_token;
-  auth.bootstrapToken(oauth_token, function(err, res) {
+  const oauth_token = qs.parse(window.location.search.replace('?', '')).oauth_token;
+  auth.bootstrapToken(oauth_token, () => {
     userStore.user = true;
     userStore.trigger(userStore.user);
-    router.run(function(Handler) {
-      /* jshint ignore:start */
-      React.render(<Handler/>, el);
-      /* jshint ignore:end */
-    });
-  });
-} else {
-  router.run(function(Handler, state) {
-    /* jshint ignore:start */
-    doAsync(state.routes, state.params).then(function() {
-      React.render(<Handler />, el);
-    }, function(err) {
-      throw err;
-    }).catch(function(e) {
-      console.error(e);
-    });
-    /* jshint ignore:end */
   });
 }
+
+render((
+  <Router
+    history={hashHistory}
+    routes={routes}
+    render={(props) => {
+      doAsync(props.routes, props.params).then();
+      return <RouterContext {...props} />;
+    }}
+  />
+), document.getElementById('app'));
