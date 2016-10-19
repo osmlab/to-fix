f28265f9338500c3e1b86314ac4691f9247db378
Ajith Ranka <ajith@mapbox.com>
2016-10-19 19:13:57 +0530

    configure redux store

---
+++ src/configureStore.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import reducer from './reducers';
+
+const configureStore = () => {
+  const middlewares = [thunk];
+  if (process.env.NODE_ENV === 'development') {
+    const logger = createLogger();
+    middlewares.push(logger);
+  }
+
+  return createStore(
+    reducer,
+    applyMiddleware(...middlewares)
+  );
+};
+
+export default configureStore;
