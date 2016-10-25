57b9e01af38c791ad62d048d71dca848c9bd7bf7
Ajith Ranka <ajith@mapbox.com>
2016-10-25 16:59:40 +0530

    refactor login component

--- src/components/shared/login.js
+++ src/components/shared/login.js
@@ -1,50 +1,67 @@
-import React from 'react';
-import Reflux from 'reflux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import * as config from '../../config';
-import userStore from '../../stores/user_store';
-import actions from '../../actions/actions';
+import { USER_PROFILE_URL } from '../../config';
+import { userLogin, getUserDetails } from '../../actions';
+import { getAuthenticated, getUsername, getOsmId, getAvatar} from '../../reducers';
 
-const Login = React.createClass({
-  mixins: [
-    Reflux.connect(userStore, 'user')
-  ],
+class Login extends Component {
+  componentDidMount() {
+    const { authenticated, getUserDetails } = this.props;
+    if (authenticated) getUserDetails();
+  }
 
-  render: function() {
-    var logState;
-    var user = this.state.user;
-    var modal = document.getElementById('modal');
-    var profile = config.userProfileURL + user.username;
+  onLoginClick = () => {
+    const { userLogin, getUserDetails } = this.props;
+    userLogin().then(getUserDetails);
+  }
 
-    if (user.auth) {
-      logState = (
+  renderLoginState() {
+    const { authenticated, username, avatar } = this.props;
+    const profileURL = `${USER_PROFILE_URL}/${username}`;
+
+    if (authenticated) {
+      return (
         <div className='pad1x col12 truncate clearfix mobile-cols'>
           <div className='pad0y col6'>
-            <a className='strong small dark' target='_blank' href={profile} title='Profile on OpenStreetMap'>
-              <img className='dot avatar' src={user.avatar} />
-              {user.username}
+            <a className='strong small dark' target='_blank' href={profileURL} title='Profile on OpenStreetMap'>
+              <img className='dot avatar' src={avatar} role='presentation' />
+              {username}
             </a>
           </div>
-          <button onClick={actions.openSettings} className='col6 icon sprocket button quiet small animate'>Settings</button>
+          <button onClick={() => console.log('openSettings')} className='col6 icon sprocket button quiet small animate'>Settings</button>
         </div>
       );
     } else {
-      logState = (
+      return (
         <div className='pad1x'>
-          <button onClick={actions.userLogin} className='icon osm button small col12 animate'>Authorize on OSM</button>
+          <button onClick={this.onLoginClick} className='icon osm button small col12 animate'>Authorize on OSM</button>
         </div>
       );
     }
-
+  }
+  render() {
     return (
       <div className='space-bottom1'>
         <h4 className='dark block pad1x space-bottom1'>Account</h4>
         <div id='user-stuff' className='space-bottom1 col12 clearfix mobile-cols'>
-          {logState}
+          {this.renderLoginState()}
         </div>
       </div>
     );
   }
+}
+
+const mapStateToProps = (state) => ({
+  authenticated: getAuthenticated(state),
+  username: getUsername(state),
+  osmid: getOsmId(state),
+  avatar: getAvatar(state),
 });
+
+Login = connect(
+  mapStateToProps,
+  { userLogin, getUserDetails }
+)(Login);
 
 export default Login;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const OAUTH_CONSUMER_KEY = 'KcVfjQsvIdd7dPd1IFsYwrxIUd73cekN1QkqtSMd';
 export const OAUTH_CONSUMER_SECRET = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
 export const TASK_SERVER_URL = 'https://build-to-fix-staging.tilestream.net';
+export const USER_PROFILE_URL = 'https://www.openstreetmap.org/user';
 
 export const accessToken = 'pk.eyJ1IjoiYWFyb25saWRtYW4iLCJhIjoiNTVucTd0TSJ9.wVh5WkYXWJSBgwnScLupiQ';
 export const raven = 'https://e45a6671d39447fda045d873eba13840@app.getsentry.com/35914';
@@ -8,4 +9,3 @@ export const bing = 'Arzdiw4nlOJzRwOz__qailc8NiR31Tt51dN2D7cm57NrnceZnCpgOkmJhNp
 export const osmApi = 'https://www.openstreetmap.org/api/0.6/';
 export const josm = 'http://localhost:8111/load_and_zoom?';
 export const iD = 'http://openstreetmap.us/iD/release/#';
-export const userProfileURL = 'https://www.openstreetmap.org/user/';
