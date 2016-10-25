df232507aef7eb10e065c907ff4c813366898aed
Ajith Ranka <ajith@mapbox.com>
2016-10-25 13:40:10 +0530

    show fetching and error state

--- src/reducers/activity.js
+++ src/reducers/activity.js
@@ -2,11 +2,25 @@ const activity = (state = {}, action) => {
   switch(action.type) {
     case 'tasks/SELECT_TASK':
       return {};
+    case 'activity/FETCH_ACTIVIY_REQUEST':
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+      };
     case 'activity/FETCH_ACTIVITY_SUCCESS':
       return {
         ...action.params,
         ...action.response,
+        isFetching: false,
+        error: null,
       };
+    case 'activity/FETCH_ACTIVITY_FAILURE':
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
     default:
       return state;
   }
--- src/reducers/stats.js
+++ src/reducers/stats.js
@@ -2,10 +2,24 @@ const stats = (state = {}, action) => {
   switch(action.type) {
     case 'tasks/SELECT_TASK':
       return {};
+    case 'stats/FETCH_STATS_REQUEST':
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+      };
     case 'stats/FETCH_STATS_SUCCESS':
       return {
         ...action.params,
         ...action.response,
+        isFetching: false,
+        error: null,
+      };
+    case 'stats/FETCH_STATS_FAILURE':
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
       };
     default:
       return state;
