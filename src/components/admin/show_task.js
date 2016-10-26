857bb6682beec044f7b6ae22a12fef52381f41cb
Ajith Ranka <ajith@mapbox.com>
2016-10-26 18:07:08 +0530

    refactor show task component

--- src/components/admin/show_task.js
+++ src/components/admin/show_task.js
@@ -1,98 +1,71 @@
 import React from 'react';
-import Reflux from 'reflux';
 import d3 from 'd3';
 
-import actions from '../../actions/actions';
-import Admin_store from '../../stores/admin_store';
+const ShowTask = ({ task }) => {
+  const dateDisplay = d3.time.format('%B %-d');
+  const timeDisplay = d3.time.format('%-I:%-M%p');
 
-const ShowTask = React.createClass({
-  mixins: [
-    Reflux.connect(Admin_store, 'task')
-  ],
-  statics: {
-    fetchData: function(params) {
-      actions.getTasks(params.task);
-    }
-  },
-  render: function() {
-    var task = this.state.task;
-    var info = '';
+  const updatedDay = dateDisplay(new Date(task.value.updated * 1000));
+  const updatedTime = timeDisplay(new Date(task.value.updated * 1000));
 
-    if (typeof task !== 'undefined') {
-      var dateDisplay = d3.time.format('%B %-d');
-      var timeDisplay = d3.time.format('%-I:%-M%p');
-      var actionDay = dateDisplay(new Date(task.updated * 1000));
-      var actionTime = timeDisplay(new Date(task.updated * 1000));
-      var status = '';
-      (task.status) ? status = 'Completed': status = 'Items remaining to be done';
-      info = ( <div>
-                  <div className='rows'>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Title</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                        {task.title}
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Id Task</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                        {task.id}
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Source</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                        {task.source}
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Description</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                        {task.description}
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Changeset comment</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                        {task.changeset_comment}
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Updated</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                      {actionDay}<span className='quiet'> {actionTime} </span>
-                      </div>
-                    </div>
-                    <div className='clearfix fill-darken1 dark mobile-cols'>
-                      <div className='fl strong pad1 fill-darken1 editor-key'>
-                        <span className='capitalize'>Status</span>
-                      </div>
-                      <div className='pad1 fl space'>
-                      {status}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-      );
-    }
+  const status = (task.isCompleted) ? 'Completed.' : 'Items remaining to be done.';
 
-    return (
-        <div>{info}</div>
-    );
-
-  }
-});
+  return (
+    <div>
+      <div className='rows'>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Title</span>
+          </div>
+          <div className='pad1 fl space'>
+            {task.value.name}
+          </div>
+        </div>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Task ID</span>
+          </div>
+          <div className='pad1 fl space'>
+            {task.idtask}
+          </div>
+        </div>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Description</span>
+          </div>
+          <div className='pad1 fl space'>
+            {task.value.description}
+          </div>
+        </div>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Changeset comment</span>
+          </div>
+          <div className='pad1 fl space'>
+            {task.value.changesetComment}
+          </div>
+        </div>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Updated</span>
+          </div>
+          <div className='pad1 fl space'>
+            {updatedDay}
+            {' '}
+            <span className='quiet'>{updatedTime}</span>
+          </div>
+        </div>
+        <div className='clearfix fill-darken1 dark mobile-cols'>
+          <div className='fl strong pad1 fill-darken1 editor-key'>
+            <span className='capitalize'>Status</span>
+          </div>
+          <div className='pad1 fl space'>
+          {status}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default ShowTask;
