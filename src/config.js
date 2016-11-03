0e8c2800ef7568bee373662b024348bddee3e089
Ajith Ranka <ajith@mapbox.com>
2016-11-03 13:12:38 +0530

    remove raven; clean up index.js

--- src/config.js
+++ src/config.js
@@ -3,9 +3,5 @@ export const OAUTH_CONSUMER_SECRET = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
 export const TASK_SERVER_URL = 'https://build-to-fix-staging.tilestream.net';
 export const USER_PROFILE_URL = 'https://www.openstreetmap.org/user';
 
-export const accessToken = 'pk.eyJ1IjoiYWFyb25saWRtYW4iLCJhIjoiNTVucTd0TSJ9.wVh5WkYXWJSBgwnScLupiQ';
-export const raven = 'https://e45a6671d39447fda045d873eba13840@app.getsentry.com/35914';
-export const bing = 'Arzdiw4nlOJzRwOz__qailc8NiR31Tt51dN2D7cm57NrnceZnCpgOkmJhNpGoppU';
-export const osmApi = 'https://www.openstreetmap.org/api/0.6/';
 export const josm = 'http://localhost:8111/load_and_zoom?';
 export const iD = 'http://openstreetmap.us/iD/release/#';
--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import { render } from 'react-dom';
-// import qs from 'querystring';
+import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
-import Raven from 'raven-js';
 
 import routes from './routes';
 import * as config from './config';
-
-import { Provider } from 'react-redux';
 import configureStore from './configureStore';
-
-Raven.config(config.raven, {
-  whitelistUrls: ['osmlab.github.io/to-fix/']
-}).install();
 
 const store = configureStore();
 
