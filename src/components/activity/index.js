52d3e1adf7fb6aa9c9dbd00acae6172a50ef681a
Ajith Ranka <ajith@mapbox.com>
2016-11-08 13:20:11 +0530

    use getStatsSummary in activity

--- src/components/activity/index.js
+++ src/components/activity/index.js
@@ -5,7 +5,7 @@ import d3 from 'd3';
 
 import { USER_PROFILE_URL } from '../../config';
 import { fetchActivity } from '../../actions';
-import { getCurrentTask, getActivityData } from '../../reducers';
+import { getCurrentTask, getActivityData, getStatsSummary } from '../../reducers';
 
 class Activity extends Component {
   state = {
@@ -17,8 +17,8 @@ class Activity extends Component {
   }
 
   fetchData() {
-    const { fetchActivity, currentTaskId, currentTask } = this.props;
-    const createdAt = currentTask.value.stats[0].date * 1000;
+    const { fetchActivity, currentTaskId, statsSummary } = this.props;
+    const createdAt = statsSummary.date * 1000;
 
     const dateFormat = d3.time.format('%Y-%m-%d');
     const _from = dateFormat(new Date(createdAt));
@@ -110,6 +110,7 @@ class Activity extends Component {
 const mapStateToProps = (state, { params }) => ({
   currentTaskId: params.task,
   currentTask: getCurrentTask(state, params.task),
+  statsSummary: getStatsSummary(state),
   activity: getActivityData(state),
 });
 
