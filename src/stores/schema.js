1b0346a63beec1c74b4990c159e61f8f7d440b1f
Ajith Ranka <ajith@mapbox.com>
2016-11-14 16:45:50 +0530

    rename schema to schemas

--- src/stores/schema.js
+++
@@ -1,7 +0,0 @@
-import { Schema, arrayOf } from 'normalizr';
-
-export const task = new Schema('tasks', { idAttribute: 'idtask' });
-export const arrayOfTasks = arrayOf(task);
-
-export const item = new Schema('items', { idAttribute: item => item.properties._key });
-export const arrayOfItems = arrayOf(item);
