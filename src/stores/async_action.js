2ad4047a1d82401cf4ee7db223dab09fc5adb9cf
Ajith Ranka <ajith@mapbox.com>
2016-11-15 15:14:25 +0530

    dispatch start/stop loading actions from async actions

--- src/stores/async_action.js
+++ src/stores/async_action.js
@@ -1,5 +1,6 @@
 import keymirror from 'keymirror';
 import { normalize } from 'normalizr';
+import LoadingActionCreators from './loading_action_creators';
 
 export const AsyncStatus = keymirror({
   REQUEST: null,
@@ -7,7 +8,7 @@ export const AsyncStatus = keymirror({
   FAILURE: null,
 });
 
-export const asyncAction = ({ type, asyncCall, responseSchema }) => {
+export const asyncAction = ({ type, asyncCall, responseSchema, showLoader = false }) => {
   return (params = {}) => (dispatch) => {
     dispatch({
       type,
@@ -15,18 +16,26 @@ export const asyncAction = ({ type, asyncCall, responseSchema }) => {
       ...params,
     });
 
+    if (showLoader) dispatch(LoadingActionCreators.startLoading());
+
     return asyncCall(params).then(
-      response => dispatch({
-        type,
-        status: AsyncStatus.SUCCESS,
-        response: responseSchema ? normalize(response, responseSchema) : response,
-        params,
-      }),
-      error => dispatch({
-        type,
-        status: AsyncStatus.FAILURE,
-        error,
-      })
+      response => {
+        dispatch({
+          type,
+          status: AsyncStatus.SUCCESS,
+          response: responseSchema ? normalize(response, responseSchema) : response,
+          params,
+        });
+        if (showLoader) dispatch(LoadingActionCreators.stopLoading());
+      },
+      error => {
+        dispatch({
+          type,
+          status: AsyncStatus.FAILURE,
+          error,
+        });
+        if (showLoader) dispatch(LoadingActionCreators.stopLoading());
+      }
     );
   };
 };
