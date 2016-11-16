1a6fd8a361f6abddda717eb5de22a1627fbcbd25
Ajith Ranka <ajith@mapbox.com>
2016-11-16 12:32:32 +0530

    clean up markup

--- src/components/admin/show_task.js
+++ src/components/admin/show_task.js
@@ -11,57 +11,55 @@ const ShowTask = ({ task }) => {
   const status = (task.isCompleted) ? 'Completed.' : 'Items remaining to be done.';
 
   return (
-    <div>
-      <div className='rows'>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Title</span>
-          </div>
-          <div className='pad1 fl space'>
-            {task.value.name}
-          </div>
+    <div className='rows'>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Title</span>
         </div>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Task ID</span>
-          </div>
-          <div className='pad1 fl space'>
-            {task.idtask}
-          </div>
+        <div className='pad1 fl space'>
+          {task.value.name}
         </div>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Description</span>
-          </div>
-          <div className='pad1 fl space'>
-            {task.value.description}
-          </div>
+      </div>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Task ID</span>
         </div>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Changeset comment</span>
-          </div>
-          <div className='pad1 fl space'>
-            {task.value.changesetComment}
-          </div>
+        <div className='pad1 fl space'>
+          {task.idtask}
         </div>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Updated</span>
-          </div>
-          <div className='pad1 fl space'>
-            {updatedDay}
-            {' '}
-            <span className='quiet'>{updatedTime}</span>
-          </div>
+      </div>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Description</span>
         </div>
-        <div className='clearfix fill-darken1 dark mobile-cols'>
-          <div className='fl strong pad1 fill-darken1 editor-key'>
-            <span className='capitalize'>Status</span>
-          </div>
-          <div className='pad1 fl space'>
+        <div className='pad1 fl space'>
+          {task.value.description}
+        </div>
+      </div>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Changeset comment</span>
+        </div>
+        <div className='pad1 fl space'>
+          {task.value.changesetComment}
+        </div>
+      </div>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Updated</span>
+        </div>
+        <div className='pad1 fl space'>
+          {updatedDay}
+          {' '}
+          <span className='quiet'>{updatedTime}</span>
+        </div>
+      </div>
+      <div className='clearfix fill-darken1 dark mobile-cols'>
+        <div className='fl strong pad1 fill-darken1 editor-key'>
+          <span className='capitalize'>Status</span>
+        </div>
+        <div className='pad1 fl space'>
           {status}
-          </div>
         </div>
       </div>
     </div>
