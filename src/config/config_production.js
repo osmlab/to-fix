be7508de3aefc018565dc405dc29b9b7eda2f957
Ajith Ranka <ajith@mapbox.com>
2016-11-16 22:52:43 +0530

    oops, wrong url

--- src/config/config_production.js
+++ src/config/config_production.js
@@ -2,7 +2,7 @@ export const OAUTH_CONSUMER_KEY = 'KcVfjQsvIdd7dPd1IFsYwrxIUd73cekN1QkqtSMd';
 export const OAUTH_CONSUMER_SECRET = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWppdGhyYW5rYSIsImEiOiJjaXA5b20zdXQwMW9xdGNseXJ6M2JpeHZlIn0.FTSj2x9tyD37djfYYWrtAw';
 
-export const TASK_SERVER_URL = 'https://build-to-fix-production.tilestream.net';
+export const TASK_SERVER_URL = 'https://build-to-fix-production.mapbox.com';
 export const USER_PROFILE_URL = 'https://www.openstreetmap.org/user';
 export const MAPBOX_GEOCODER_URL = 'https://api.mapbox.com/geocoding/v5';
 export const JOSM_RC_URL = 'https://localhost:8112/load_and_zoom';
