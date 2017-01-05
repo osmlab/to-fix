fb7c7cc2d228846c4b9fe0195d092888836a8f01
Ajith Ranka <ajith@mapbox.com>
2017-01-05 15:55:51 +0530

    user can login or logout

--- src/constants/user_constants.js
+++ src/constants/user_constants.js
@@ -1,9 +1,8 @@
 import keymirror from 'keymirror';
 
 const UserConstants = keymirror({
-  USER_LOGIN: null,
-  USER_FETCH_USER_DETAILS: null,
-  USER_LOGOUT: null,
+  USER_LOGGED_IN: null,
+  USER_LOGGED_OUT: null,
 });
 
 export default UserConstants;
