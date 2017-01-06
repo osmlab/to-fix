2915b1a33d4922b38a1e6f60e8a2f6a35cdfe4be
Ajith Ranka <ajith@mapbox.com>
2017-01-06 12:31:33 +0530

    read optional headers

--- src/api/utils/index.js
+++ src/api/utils/index.js
@@ -6,12 +6,18 @@ export const fetchJSON = (url, options = {}) => (
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
+      ...options.headers,
     },
   })
   .then(toJSON)
 );
 
 export const fetchForm = (url, options = {}) => (
-  fetch(url, { ...options })
+  fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+    }
+  })
     .then(toJSON)
 );
