4fc6a19eb768b6b92dbfb01d61da96d57b49a019
Ajith Ranka <ajith@mapbox.com>
2017-01-24 19:26:18 +0530

    task actions/selectors

--- src/stores/tasks_action_creators.js
+++ src/stores/tasks_action_creators.js
@@ -1,4 +1,4 @@
-import api from '../api';
+import { server } from '../services';
 import schemas from './schemas';
 import { asyncAction } from './async_action';
 import TasksConstants from '../constants/tasks_constants';
@@ -6,14 +6,14 @@ import TasksConstants from '../constants/tasks_constants';
 const TasksActionCreators = {
   fetchAllTasks: asyncAction({
     type: TasksConstants.TASKS_FETCH_ALL,
-    asyncCall: api.tofix.fetchAllTasks,
+    asyncCall: server.fetchAllTasks,
     responseSchema: { tasks: schemas.arrayOfTasks },
     showLoader: true,
   }),
 
   fetchTaskById: asyncAction({
     type: TasksConstants.TASKS_FETCH_BY_ID,
-    asyncCall: api.tofix.fetchTaskById,
+    asyncCall: server.fetchTaskById,
     responseSchema: schemas.task,
   }),
 
@@ -24,21 +24,21 @@ const TasksActionCreators = {
 
   createTask: asyncAction({
     type: TasksConstants.TASKS_CREATE,
-    asyncCall: api.tofix.createTask,
+    asyncCall: server.createTask,
     responseSchema: schemas.task,
     showLoader: true,
   }),
 
   updateTask: asyncAction({
     type: TasksConstants.TASKS_UPDATE,
-    asyncCall: api.tofix.updateTask,
+    asyncCall: server.updateTask,
     responseSchema: schemas.task,
     showLoader: true,
   }),
 
   destroyTask: asyncAction({
     type: TasksConstants.TASKS_DESTROY,
-    asyncCall: api.tofix.destroyTask,
+    asyncCall: server.destroyTask,
     showLoader: true,
   }),
 };
