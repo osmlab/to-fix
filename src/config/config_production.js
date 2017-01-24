2181d9b44ac6c5b620d75fab8a37d49c6d0de2cb
Ajith Ranka <ajith@mapbox.com>
2017-01-24 19:20:09 +0530

    remove osm consumer and secret keys

--- src/config/config_production.js
+++ src/config/config_production.js
@@ -1,5 +1,3 @@
-export const OAUTH_CONSUMER_KEY = 'KcVfjQsvIdd7dPd1IFsYwrxIUd73cekN1QkqtSMd';
-export const OAUTH_CONSUMER_SECRET = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWppdGhyYW5rYSIsImEiOiJjaXA5b20zdXQwMW9xdGNseXJ6M2JpeHZlIn0.FTSj2x9tyD37djfYYWrtAw';
 
 export const TASK_SERVER_URL = 'https://build-to-fix-production.mapbox.com';
