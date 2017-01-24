aa6948a8100500af8b6fdbe068f044bbe3ec0e4f
Ajith Ranka <ajith@mapbox.com>
2017-01-24 19:23:45 +0530

    clean up activity actions and reducer

--- src/stores/activity_action_creators.js
+++ src/stores/activity_action_creators.js
@@ -1,17 +1,11 @@
-import api from '../api';
+import { server } from '../services';
 import { asyncAction } from './async_action';
 import ActivityConstants from '../constants/activity_constants';
 
 const ActivityActionCreators = {
-  fetchAllActivity: asyncAction({
-    type: ActivityConstants.ACTIVITY_FETCH_ALL,
-    asyncCall: api.tofix.fetchAllActivity,
-    showLoader: true,
-  }),
-
-  fetchUserActivity: asyncAction({
-    type: ActivityConstants.ACTIVITY_FETCH_FOR_USER,
-    asyncCall: api.tofix.fetchUserActivity,
+  fetchRecentActivity: asyncAction({
+    type: ActivityConstants.ACTIVITY_FETCH_RECENT,
+    asyncCall: server.fetchRecentActivity,
     showLoader: true,
   }),
 };
