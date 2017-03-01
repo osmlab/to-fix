fd4cc6c1d64d821234222f8d8b89194d7c914b0f
Ajith Ranka <ajith@mapbox.com>
2017-03-01 12:06:29 +0530

    render a create task button for admins

--- src/components/app/app_sidebar.js
+++ src/components/app/app_sidebar.js
@@ -2,17 +2,38 @@ import React, { Component, PropTypes } from 'react';
 import { Link, withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
+import { ROLES } from '../../constants/user_constants';
+import UserSelectors from '../../stores/user_selectors';
 import TasksSelectors from '../../stores/tasks_selectors';
 import SettingsSelectors from '../../stores/settings_selectors';
 
 const mapStateToProps = (state, { routes }) => ({
   topLevelRoute: routes[1].name,
+  role: UserSelectors.getRole(state),
   sidebar: SettingsSelectors.getSidebarSetting(state),
   activeTasks: TasksSelectors.getActiveTasks(state),
   completedTasks: TasksSelectors.getCompletedTasks(state),
 });
 
 class Sidebar extends Component {
+  renderCreateTaskBtn() {
+    const { role, topLevelRoute } = this.props;
+
+    if (role === ROLES.ADMIN || role === ROLES.SUPERADMIN) {
+      if (topLevelRoute === "admin") {
+        return (
+          <div className='pad1x space-bottom1'>
+            <button className='button icon plus quiet truncate col12 animate'>
+              Create a new task
+            </button>
+          </div>
+        );
+      }
+    }
+
+    return null;
+  }
+
   renderTaskList(tasks) {
     const { topLevelRoute } = this.props;
 
@@ -37,12 +58,14 @@ class Sidebar extends Component {
     const isActive = sidebar ? 'active' : '';
     const sidebarClass = `sidebar pin-bottomleft clip col2 animate offcanvas-left fill-navy space-top6 ${isActive}`;
 
+    const createTaskBtn = this.renderCreateTaskBtn();
     const activeTasksList = this.renderTaskList(activeTasks);
     const completedTasksList = this.renderTaskList(completedTasks);
 
     return (
       <div className={sidebarClass}>
         <div className='scroll-styled pad2y'>
+          {createTaskBtn}
           <h4 className='dark block pad1x space-bottom1'>Current Tasks</h4>
           <nav className='dark space-bottom2'>{activeTasksList}</nav>
           <h4 className='dark block pad1x space-bottom1'>Completed Tasks</h4>
@@ -55,6 +78,7 @@ class Sidebar extends Component {
 
 Sidebar.propTypes = {
   topLevelRoute: PropTypes.string.isRequired,
+  role: PropTypes.string,
   sidebar: PropTypes.bool.isRequired,
   activeTasks: PropTypes.array.isRequired,
   completedTasks: PropTypes.array.isRequired,
