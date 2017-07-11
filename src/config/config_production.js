4dfe25bc67be48b65eac8d3b0e0f940348a116ab
Ajith Ranka <ajith@mapbox.com>
2017-07-11 22:37:20 +0530

    temporary: move josm rc to http

--- src/config/config_production.js
+++ src/config/config_production.js
@@ -3,5 +3,5 @@ export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWppdGhyYW5rYSIsImEiOiJjaXA5b20zd
 export const TASK_SERVER_URL = 'https://build-to-fix-production.mapbox.com';
 export const USER_PROFILE_URL = 'https://www.openstreetmap.org/user';
 export const MAPBOX_GEOCODER_URL = 'https://api.mapbox.com/geocoding/v5';
-export const JOSM_RC_URL = 'https://localhost:8112';
+export const JOSM_RC_URL = 'http://localhost:8111';
 export const ID_URL = 'http://preview.ideditor.com/release';
